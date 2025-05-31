f1a8e50c9ab22930fb3535af9afce7d0fe95c0c0
Uzrakhan <uzrakhan539@gmail.com>
2025-05-31 16:38:07 +0530

    Fix: upadte socket connection to Renderbackend

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,19 +1,6 @@
 import { io } from 'socket.io-client';
 
-/*
-export const socket = io('http://localhost:5000', {
-    withCredentials: true,
-    autoConnect: true
-});
-*/
-
-//use different path in development, will be different in production
-const SOCKET_URL = import.meta.env.DEV
-? 'http://localhost:5000'
-: 'https://idea-board-virid.vercel.app/';
-
-
-export const socket = io(SOCKET_URL, {
+export const socket = io('https://ideaboard-backend.onrender.com', {
   withCredentials: true,
   autoConnect: true,
   transports: ['websocket', 'polling'] 
